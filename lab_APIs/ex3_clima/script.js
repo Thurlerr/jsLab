4bae75a9eb574ed993288ae06b6390784c9f18a1
Thurlerr <andrew_thurler@hotmail.com>
2025-02-06 14:29:55 -0300

    fazer html baseado nos nomes de classes da funcao preencherFicha

---
+++ lab_APIs/ex3_clima/script.js
@@ -0,0 +1,13 @@
+// Pesquisa de Clima com Funções Assíncronas  
+  
+// API: OpenWeatherMap (https://openweathermap.org)
+
+//   Crie uma função assíncrona que faça uma requisição à OpenWeatherMap para buscar a previsão do tempo de uma cidade específica. Exiba informações como temperatura, umidade e condição atual.  
+
+// Use .filter() para exibir apenas condições climáticas extremas (temperatura acima de 35°C ou abaixo de 5°C).
+
+// Envie a URL para o site funcionando com a resposta
+// (2 Pontos)
+
+//como fazer a requisição usando nomes de cidades e não latitude/longitude?
+
